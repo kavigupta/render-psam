f67c91368ed5b2be9e82d46493930e43e9a17fca
Kavi Gupta <kavi@berkeley.edu>
2025-01-30 14:49:06 -0500

    add tests for render_psams

--- tests/test_render_psam.py
+++ tests/test_render_psam.py
@@ -1,12 +1,13 @@
+import string
 import unittest
 
 import numpy as np
 from matplotlib import pyplot as plt
 from parameterized import parameterized
 
-from render_psam import render_psam
+from render_psam import render_psam, render_psams
 
-is_testing = True
+is_testing = False
 
 
 def matplotlib_to_array(fig):
@@ -29,7 +30,7 @@ class CreatePSAMTests(unittest.TestCase):
         else:
             plt.imsave(output_path, array_contents)
 
-    def ensure_is_testing(self):
+    def test_is_testing(self):
         if not is_testing:
             raise ValueError("This test is not running in testing mode.")
 
@@ -40,7 +41,7 @@ class CreatePSAMTests(unittest.TestCase):
             ("info",),
         ]
     )
-    def test_basic_mode(self, mode):
+    def test_mode(self, mode):
         psam = np.array(
             [
                 [1, 0, 0, 0],
@@ -51,3 +52,43 @@ class CreatePSAMTests(unittest.TestCase):
         )
         render_psam(psam, psam_mode=mode)
         self.check("basic_" + mode)
+
+    def test_colors(self):
+        psam = np.array(
+            [
+                [1, 0, 0, 0],
+                [0, 2, 0, 0],
+                [3, 0, 2, 0],
+                [0, 1, 0, 1],
+            ]
+        )
+        render_psam(
+            psam,
+            color_scheme={
+                "A": "purple",
+                "C": "pink",
+                "G": "aqua",
+                "T": "orange",
+            },
+        )
+        self.check("colors")
+
+    @parameterized.expand(
+        [
+            (axis_mode, ncols, same_ylim)
+            for axis_mode in ["just_y", "completely_blank"]
+            for ncols in [1, 6]
+            for same_ylim in [True, False]
+        ]
+    )
+    def test_multiple_psams(self, axis_mode, ncols, same_ylim):
+        render_psams(
+            np.random.RandomState(0).rand(20, 10, 4),
+            names=string.ascii_uppercase[:20],
+            axes_mode=axis_mode,
+            ncols=ncols,
+            psam_mode="info",
+            same_ylim=same_ylim,
+        )
+
+        self.check(f"multiple_psams_{axis_mode}_{ncols}cols_{same_ylim=}")
